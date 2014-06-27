3a4bd543844b57c9aefb061435b01f9d2c6e6b6d
Eugene Scherba <escherba@livefyre.com>
2014-06-27 15:54:20 -0700

    allow for Chi-sq feature selection

--- classify.py
+++ classify.py
@@ -77,8 +77,6 @@ data_train, data_test = get_data_frames(
     opts.data_dir,
     lambda line: json.loads(line)['content'])
 
-categories = data_train.target_names    # for case categories == None
-
 
 # split a training set and a test set
 y_train, y_test = data_train.target, data_test.target
@@ -105,6 +103,14 @@ duration = time() - t0
 print("n_samples: %d, n_features: %d" % X_test.shape)
 print()
 
+
+# mapping from integer feature name to original token string
+if opts.use_hashing:
+    feature_names = None
+else:
+    feature_names = np.asarray(vectorizer.get_feature_names())
+
+
 if opts.select_chi2:
     print("Extracting %d best features by a chi-squared test" %
           opts.select_chi2)
@@ -112,20 +118,10 @@ if opts.select_chi2:
     ch2 = SelectKBest(chi2, k=opts.select_chi2)
     X_train = ch2.fit_transform(X_train, y_train)
     X_test = ch2.transform(X_test)
+    feature_names = ch2.transform(feature_names)[0]
     print("done in %fs" % (time() - t0))
     print()
 
-
-def trim(s):
-    """Trim string to fit on terminal (assuming 80-column display)"""
-    return s if len(s) <= 80 else s[:77] + "..."
-
-
-# mapping from integer feature name to original token string
-if opts.use_hashing:
-    feature_names = None
-else:
-    feature_names = np.asarray(vectorizer.get_feature_names())
 
 
 ###############################################################################
@@ -153,7 +149,7 @@ def benchmark(clf, clf_descr=None):
 
         if opts.top_terms is not None and feature_names is not None:
             print("top %d keywords per class:" % opts.top_terms)
-            for i, category in enumerate(categories[1:]):
+            for i, category in enumerate(data_train.target_names[1:]):
                 top_terms = np.argsort(clf.coef_[i])[-opts.top_terms:]
                 if clf.__class__.__name__.startswith("FeatureSelect"):
                     tfnames = clf.transformer_.transform(feature_names)[0]
@@ -165,7 +161,7 @@ def benchmark(clf, clf_descr=None):
     if opts.print_report:
         print("classification report:")
         print(metrics.classification_report(y_test, pred,
-                                            target_names=categories))
+                                            target_names=data_train.target_names))
 
     if opts.print_cm:
         print("confusion matrix:")
@@ -251,7 +247,8 @@ for penalty in ["l2", "l1"]:
 print('=' * 80)
 print("SGD with elasticnet penalty")
 results.append(benchmark(
-    SGDClassifier(loss='hinge', alpha=3e-5, n_iter=50, penalty='elasticnet', l1_ratio=0.3),
+    SGDClassifier(loss='hinge', alpha=3e-5, n_iter=50, penalty='elasticnet',
+                  l1_ratio=0.3),
     "SGD (elasticnet penalty)"))
 
 # Train SGD with L1-feature selection
