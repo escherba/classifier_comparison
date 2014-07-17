d664f83250cf43634067acd5dd062ff4d8741314
Alexander Rao <arao@livefyre.com>
2014-07-17 16:03:14 -0700

    first commit

--- classify.py
+++ classify.py
@@ -141,6 +141,7 @@ elif opts.vectorizer == "tfidf":
     vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.3,
                                  stop_words='english')
 
+
 content_pipeline = FeaturePipeline([
     ('cont1', TextExtractor('content')),
     ('vec', vectorizer),
@@ -176,6 +177,7 @@ if opts.vectorizer == "hashing":
     X_train = preprocess.transform(data_train.data)
 elif opts.vectorizer == "tfidf":
     X_train = preprocess.fit_transform(data_train.data)
+
 
 duration = time() - t0
 print("X_train: n_samples: %d, n_features: %d" % X_train.shape)
